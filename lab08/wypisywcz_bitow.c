0fcf19e96e54f2dae4cef08751b3dc750a743dbf
LukaszStr <lukastrzelec@gmail.com>
2017-11-29 19:34:00 +0100

    Dodano float

--- lab08/wypisywcz_bitow.c
+++ lab08/wypisywcz_bitow.c
@@ -5,73 +5,69 @@
 # include <stdlib.h>
 
 int main (int wybor, char ** argv) {
-int i,j,k,e; /* i,j - zmienne pomocnicze do pętli. k - zmienna do wyboru działania programu, e - zmienna do odpowiedniego umiejscowienia wartości bitu w tabeli */
+
 if (wybor < 2)
 {
        printf("Error \nPodaj (jako argumenty wywolania programu) czego bity chcesz wypisac:\n 0 -- znak(ASCII)\n 1 -- liczba całkowita\n 2 -- liczba typu float\n 3 -- liczba typu double \n");
 return -1;
 }
-else {
-k = atoi(argv[1]);
-int tablica[32];
-union u {char z; int i; float f; double d;} u;
-unsigned char *wsk_bajtu ;
-wsk_bajtu = (unsigned char *) &u.i ;
 
- if (k == 0)
- {
-     printf("Wpisz jakiś znak:\t");
-    scanf("%c", &u.z);
-    printf("Wprowadzono:\t%c\n\t", u.z);
-    for (i = 7; i >= 0; i--)
-    {
-              printf("%d", get_bit(u.z,i));
-    }
-}
-else if(k == 1)
+else 
 {
-    printf("Podaj jakas liczbe calkowita:\t");
-    scanf("%d", &u.i);
-    printf("Wprowadzono:\t%d\n", u.i);	
-	for (j = 0; j <= 3; j++ ) 
-	{
-	for (i=0; i<=7; i++)
-  		{
-	e = i + j*8;
-	tablica[e] = get_bit(*wsk_bajtu, i); 
-	/* printf(" %d", get_bit(*wsk_bajtu, i)); 
-	 * printf("%d", tablica[e]); 			*/ 
-  		}
-	*wsk_bajtu++;
-	}
-for (i=31; i >= 0; i--) 
+int i,j,e,k,z; /* i,j - zmienne pomocnicze do pętli. k - zmienna do wyboru działania programu, e - zmienna do odpowiedniego umiejscowienia wartości bitu w tabeli */
+k = atoi(argv[1]);
+int tablica[32];	
+union u {char z; int i; float f; double d;} u;
+if (k == 0) 
 {
-	printf("%d", tablica[i]);
+	printf("Podaj jakis znak:\t");
+	scanf("%c", &u.z);
+	z = sizeof(u.z);
 }
-}
-else if (k == 2)
+else if (k == 1) 
 {
-    printf("Podaj liczbe typu float:\t");
-    scanf("%f", &u.f);
-    printf("Wprowadzono: \t%f\n", u.f);
-  /*  int *b=0;
-    b = &u.f;  */
-    for (i=31; i>=0; i--)
-    {
-        printf("%d", get_bit(u.f, i));
-    }
+	printf("Podaj jakas liczbe calkowita:\t");
+    	scanf("%d", &u.i);
+	z = sizeof(u.i);
+}
+
+else if (k == 2) 
+{
+	printf("Podaj jakas liczbe typu float:\t");
+    	scanf("%f", &u.f);
+	z = sizeof(u.f);
 }
 else if (k == 3)
 {
-    printf("Podaj liczbe typu double:\t");
-    scanf("%lf", &u.d);
-    printf("\n");
+	printf("Podaj jakas liczbe typu double:\t");
+    	scanf("%lf", &u.d);
+	z = sizeof(u.d);
 }
 else
 {
     printf("Nieprawidlowy wybor\n");
     return -1;
 }
+unsigned char *wsk_bajtu ;
+wsk_bajtu = (unsigned char *) &u ;
+
+printf("rozmiar:\t%d bajt\n", z);
+	for (j = 0; j < z; j++ ) 
+	{
+	for (i=0; i<=7; i++)
+  		{
+	e = i + j*8;
+	tablica[e] = get_bit(*wsk_bajtu, i); 
+	/* printf(" %d", get_bit(*wsk_bajtu, i)); 
+	 printf("%d", tablica[e]); 			*/ 
+  		}
+	printf(" ");
+	*wsk_bajtu++;
+	}
+ for (i=(z*8-1); i >= 0; i--) 
+{
+	printf("%d", tablica[i]);
+}	
 }
 printf("\n");
 return 0;
