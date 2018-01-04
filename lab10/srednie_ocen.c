8bb98a6b1851f5d4005c76f515a78b775d1d852c
LukaszStr <lukastrzelec@gmail.com>
2018-01-04 10:34:28 +0100

    Ogarniecie normalizacji +-

--- lab10/srednie_ocen.c
+++ lab10/srednie_ocen.c
@@ -5,7 +5,16 @@
 # include <string.h>
 # include <errno.h>
 
-struct student { char imie [30]; char nazwisko [32]; char ocena [30][4]; int liczba_ocen;} student;
+/* Struktura studenta i bufor*/
+struct student { char imie [30]; char nazwisko [32]; char ocena [30][4]; int liczba_ocen;} Student;
+char bufor[32];
+
+/*Funkcja normalizująca nazwisko i imię na polski */
+char normalizuj (char bufor [30])	{
+if (bufor[0] > 90) {
+	bufor[0] -= 32;
+}
+}
 
 int main (int argc, char** argv) {
 
@@ -15,9 +24,9 @@ if (argc<2) {
   exit(-1);
 }
 
-/* grupa[30]; */
+struct student grupa[30];
 /* deklaracja iteratora i, liczby ocen, zmiennej ino - sprawdzającej czy w danej linii pliku znajdują się 3 byty */
-int i=0; int liczba_ocen=0; int ino = 1;
+int i=0; int liczba_ocen=0; int ino = 1; int licznik_student=0;
 for (i=1; i<=argc; i++) {
 FILE *plik;
 plik = fopen(argv[1], "r");
@@ -29,20 +38,23 @@ if (!plik)
        exit(-2);
    }
 else {
-    else{
   while (ino != EOF)
     {
-      ino = fscanf(plik, "%s %s %s\n", student.imie, student.nazwisko, student.ocena[1]);
-      printf("Imie: %s\nNazwisko: %s\nOcena: %s\n liczba wczt. danych=%d\n", student.imie, student.nazwisko, student.ocena[1], ino);
+      ino = fscanf(plik, "%s %s %s\n", Student.imie, Student.nazwisko, Student.ocena[1]);
+      printf("Imie: %s\nNazwisko: %s\nOcena: %s\n liczba wczt. danych=%d\n", Student.imie, Student.nazwisko, Student.ocena[1], ino);
 
 /*sprawdz czy dana linia zawiera komplet danych */
       if (ino < 3) {
         fprintf(stderr, " \n Blad.Ta linia nie zawiera kompletnych danych. Pomijam\n");
       }
+
       else{
-        student s1
+        normalizuj(Student.imie);
+        normalizuj(Student.nazwisko);
+        strcpy(grupa[licznik_student].imie, Student.imie);
+        strcpy(grupa[licznik_student].nazwisko, Student.nazwisko);
+        printf("\t Dane studenta to: %s %s\n",grupa[licznik.student].imie, grupa[licznik.student].nazwisko);
       }
-  }
 }
   fclose(plik);
 }
