a7513b25f2b4dfeb8a4119866486f0ef0c480006
LukaszStr <lukastrzelec@gmail.com>
2018-01-11 00:32:08 +0100

    Dziala sortowanie (sort of) Problem z ocenami, czasem sie kopiuja

--- lab10/srednie_ocen.c
+++ lab10/srednie_ocen.c
@@ -8,6 +8,7 @@
 /* Struktura studenta i bufor do normalizacji*/
 struct student { char imie [30]; char nazwisko [32]; char ocena [30][4]; int liczba_ocen;} student;
 struct student grupa[30];
+struct student zeruj = {0};
 char bufor[32];
 double oceny_grupy[];
 
@@ -18,25 +19,38 @@ if (bufor[0] > 90) {
 	bufor[0] -= 32;
 }
 else {}
-/* Zamień kolejne litery imienia i nazwiska na małe znaki */
-for (i=1; i<strlen(bufor); i++) {
-	if (bufor[i] < 97) {
-		bufor[i] += 32;
+	/* Zamień kolejne litery imienia i nazwiska na małe znaki */
+	for (i=1; i<strlen(bufor); i++) {
+		if (bufor[i] < 97) {
+			bufor[i] += 32;
+		}
 	}
-}
 return 0;
 } /* Koniec funkcji normalizuj */
 
 /* funkcja porównująca */
-int compare (const void *s1, const void *s2 ) {
+int compare (const void *p, const void *q ) {
 int i;
-const struct student *student_s1 = s1;
-const struct student *student_s2 = s2;
-i = strcmp(student_s1->nazwisko, student_s2->nazwisko);
+struct student *student_s1 = (struct student *)p;
+struct student *student_s2 = (struct student *)q;
+i = strncmp(student_s1->nazwisko, student_s2->nazwisko, 4);
 if (!i) return 1;
-return strcmp(student_s1->imie, student_s2->imie);
+return strncmp(student_s1->nazwisko, student_s2->nazwisko, 4);
 }
 
+/* Konwersja oceny na typ double. Sprawdź czy jest znak + albo minus */
+double konwersja_oceny (const char *ocena_x) {
+	char *reszta_z_konwersji;
+	double plus_minus;
+	if(strchr(ocena_x, '+')) {
+		plus_minus=0.25;
+		}
+	if (strchr(ocena_x, '-')) {
+		plus_minus=-0.25;
+		}
+return ( strtod(ocena_x, &reszta_z_konwersji) + plus_minus);
+/* plus_minus =0; */
+}
 
 
 /* Początek funkcji main 	*/
@@ -49,8 +63,8 @@ if (argc<2) {
   exit(-1);
 }
 
-/* deklaracja iteratów pomocniczych i,j, liczby ocen, zmiennej ino - sprawdzającej czy w danej linii pliku znajdują się 3 byty, liczników itp, plus_minus - do konwersji oceny */
-int i=0; int j=0; int liczba_ocen=0; int ino = 1; int licznik_student=0; int liczba_studentow=0; int licznik_powtarzajacych=0; int iterator_plikow; double plus_minus;
+/* deklaracja iteratów pomocniczych i,j, liczby ocen, zmiennej ino - sprawdzającej czy w danej linii pliku znajdują się 3 byty, liczników itp,  */
+int i=0; int j=0; int liczba_ocen=0; int ino = 1; int licznik_student=0; int liczba_studentow=0; int licznik_powtarzajacych=0; int iterator_plikow;
 /* Pętla po plikach: */
 for (iterator_plikow=1; iterator_plikow<argc; iterator_plikow++)
 {
@@ -106,50 +120,30 @@ plik = fopen(argv[iterator_plikow], "r");
 					strcpy(grupa[licznik_student].nazwisko, student.nazwisko);
 					strcpy(grupa[licznik_student].ocena[(grupa[licznik_student].liczba_ocen)], student.ocena[1]);
 
-/* Konwersja oceny na typ double. Sprawdź czy jest znak + albo minus */
-					char *reszta_z_konwersji;
-					if(strchr(student.ocena[1], '+')) {
-						plus_minus=0.25;
-					}
-					if (strchr(student.ocena[1], '-')) {
-						plus_minus=-0.25;
-					}
-					/* Dodaj plus_minus do oceny skonwertowanej do double */
-					oceny_grupy[liczba_studentow] = ( strtod(student.ocena[1], &reszta_z_konwersji) + plus_minus);
-					plus_minus = 0; /* wyzerować licznik */
-
 /* Koniec przebiegu pętli, zinkrementuj liczniki */
 					grupa[licznik_student].liczba_ocen += 1;
 					liczba_studentow += 1;
 					licznik_student = (liczba_studentow - licznik_powtarzajacych);
 					liczba_ocen += 1;
+/* i wyzeruj na wszelki wypadek */
+					student = zeruj;
 			}
       } /* Koniec działań na danym pliku z danymi */
 			ino = 0; /* ustawia ino na coś innego niż EOF, żeby wejść w pętlę */
+			student = zeruj;
 		}
-/* Wyświetlanie tabeli */
-for (i=0; i< liczba_studentow; i++) {
-	fprintf(stdout, "%s\t\t%s\t\t%s\t%f\n",grupa[i].nazwisko, grupa[i].imie, grupa[i].ocena,i);
-}
-qsort(grupa, sizeof(student), liczba_studentow, compare);
+/*Sortowanie i Wyświetlanie tabeli */
+qsort(grupa, liczba_studentow, sizeof(student), compare);
 fprintf(stdout, "Tabelka dla pliku: %s\n", argv[iterator_plikow] );
 fprintf(stdout, "Nazwisko:\t\tImię:\t\tLista ocen:\tŚrednia ocen:\n");
-for (i=0; i< liczba_studentow; i++) {
-	fprintf(stdout, "%s\t\t%s\t\t%s\t%f\n",grupa[i].nazwisko, grupa[i].imie, grupa[i].ocena,i);
+for (i=(0+licznik_powtarzajacych); i< liczba_studentow; i++) {
+/*  double
+	double srednia_studenta = */
+	fprintf(stdout, "%15s\t\t%15s\t\t%12s\t%.2f\n",grupa[i].nazwisko, grupa[i].imie, grupa[i].ocena, oceny_grupy[i]);
 }
   fclose(plik);
+	liczba_studentow = 0; licznik_student =0; licznik_powtarzajacych = 0; liczba_ocen=0;
 }
-/* ustawia licznik studentów na zgodny z liczbą osób */
-liczba_studentow = liczba_studentow - licznik_powtarzajacych;
- /*for (i=0; i < liczba_studentow; i++) {
-	for (j=0; j < grupa[i].liczba_ocen;j++){
-fprintf(stdout, "\t Imię: %s\tNazwisko: %s\n\t Liczba ocen: %d",grupa[i].imie, grupa[i].nazwisko, grupa[i].liczba_ocen);
-fprintf(stdout, "\tOceny: %s\n", grupa[i].ocena[j]);
-			}
-		}
-		for (i=0; i<liczba_ocen ; i++) {
-			fprintf(stdout, "oceny_grupy:%f\n",oceny_grupy[i]);
-		}
-		*/
+
 return 0;
 }
