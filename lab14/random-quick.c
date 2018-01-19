665ef92eace648862c456fa1be79cafb64684a0a
LukaszStr <lukastrzelec@gmail.com>
2018-01-19 20:32:45 +0100

    Nothing really

--- lab14/random-quick.c
+++ lab14/random-quick.c
@@ -5,64 +5,55 @@
 void quickSort( int[], int, int);
 int partition( int[], int, int);
 
-int main() 
+int main()
 {
-int n=500000;
+int n=500000/2;
 int table[n], i, r;
 clock_t start, stop, czas_wykonania, czas_sec;
 srand(time(NULL));
-start = clock();
-
-	
 
 for (i=0; i<n; i++) {
  r=rand()%101;
  table[i]=r;
 }
-stop = clock();	
-	czas_wykonania = (stop - start) ;
-	czas_sec = (long double)(czas_wykonania * 1000 / (CLOCKS_PER_SEC)) ;
-printf("Start T: %zu End T: %zu \t Czas Wykonania: %Lf \n ",start, stop, czas_sec);
 	/* for (i=0; i<500; i++) {
  	printf("%i\n",table[i]);
-	} */	
+	} */
 	start = clock();
 	quickSort( table, 0, n);
-	stop = clock();	
+	stop = clock();
 	czas_wykonania = (stop - start) ;
 	czas_sec = (long double)(czas_wykonania * 1000 / (CLOCKS_PER_SEC)) ;
-	 printf ("Od startu zegara uplynelo %ld jednostek\n", clock ());
-    printf ("na jedna sekunde sklada sie %ld jednostek\n", CLOCKS_PER_SEC);
-	printf("Start T: %d End T: %d \t Czas Wykonania: %Ld \n ",start, stop, czas_sec);
-	 for(i = 0; i < 501; ++i) {
+	printf ("Od startu zegara uplynelo %ld jednostek\n", clock ());
+  printf ("na jedna sekunde sklada sie %ld jednostek\n", CLOCKS_PER_SEC);
+	printf("Start T: %ld End T: %ld \t Czas Wykonania: %ld [ms]\n ",start, stop, czas_sec);
+	/*  for(i = 0; i < 501; ++i) {
 		printf(" %d ", table[i]);
-	}
+	} */
 	printf("\n");
-return 0;
 
+return 0;
 }
 
 void quickSort( int a[], int l, int r)
 {
    int j;
 
-   if( l < r ) 
+   if( l < r )
    {
    	/* divide and conquer */
         j = partition( a, l, r);
        quickSort( a, l, j-1);
        quickSort( a, j+1, r);
    }
-	
+
 }
-
-
 
 int partition( int a[], int l, int r) {
    int pivot, i, j, t;
    pivot = a[l];
    i = l; j = r+1;
-		
+
    while( 1)
    {
    	do ++i; while( a[i] <= pivot && i <= r );
