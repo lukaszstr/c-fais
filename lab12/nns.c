27f6e3cdd8952d4879ca666a7c1f6b7e0216eadd
LukaszStr <lukastrzelec@gmail.com>
2018-01-23 20:49:33 +0100

    Started work with nns. Creating drand48

--- lab12/nns.c
+++ lab12/nns.c
@@ -1,19 +1,27 @@
 /* Łukasz Strzelec */
 
+#define _XOPEN_SOURCE  /* Dołącza funkcje z POSIX i innych? */
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-int main (int argc, int** argv) {
+int main (int argc, char** argv) {
 if (argc<3) {
   printf("Niepoprawny sposob wywolania programu. Jako argumenty wywolania podaj: 1) liczbe liczb losowych 2) rozmiar macierzy n 3) sposob dzialania programu: 0/1 (0-srand48, 1-rand)");
 }
 else {
-int i;
+int liczba_losowych, rozmiar_macierzy, mode, i; /* mode - sposób działania programu */
+double macierz[rozmiar_macierzy], r;
+char *error_argv1, *error_argv2, *error_argv3;
 time_t t;
-  srand48(time(&t));
-printf("liczba= %d\n", drand48());
+srand48(time(NULL));
+liczba_losowych = strtol(argv[1], &error_argv1, 10 );
+
+for (i=0; i<liczba_losowych; i++) {
+  r = drand48();
+  macierz[i] = r;
 }
 
+}
 return 0;
 }
