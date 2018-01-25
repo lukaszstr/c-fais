56db978f9d4e18d6e66aac4a3d0f9f48b4f6f850
LukaszStr <lukastrzelec@gmail.com>
2018-01-25 10:20:50 +0100

    Zaimplementowano na bazie nns.c random-quick

--- lab14/quicksort.c
+++ lab14/quicksort.c
@@ -1,10 +1,12 @@
+/* Łukasz Strzelec */
+
 #include <stdio.h>
 
 void quickSort( int[], int, int);
 int partition( int[], int, int);
 
 
-int main() 
+int main()
 {
 	int a[] = { 7, 12, 1, -2, 0, 15, 4, 11, 9};
 
@@ -29,14 +31,14 @@ void quickSort( int a[], int l, int r)
 {
    int j;
 
-   if( l < r ) 
+   if( l < r )
    {
    	/* divide and conquer */
         j = partition( a, l, r);
        quickSort( a, l, j-1);
        quickSort( a, j+1, r);
    }
-	
+
 }
 
 
@@ -44,7 +46,7 @@ int partition( int a[], int l, int r) {
    int pivot, i, j, t;
    pivot = a[l];
    i = l; j = r+1;
-		
+
    while( 1)
    {
    	do ++i; while( a[i] <= pivot && i <= r );
--- lab14/random-quick.c
+++ lab14/random-quick.c
@@ -3,37 +3,123 @@
 #include <time.h>
 
 void quickSort( int[], int, int);
-int partition( int[], int, int);
+int podziel( int[], int, int);
 
-int main()
-{
-int n=500000/2;
-int table[n], i, r;
-clock_t start, stop, czas_wykonania, czas_sec;
-srand(time(NULL));
+/* funkcja porównująca dla quicksorta */
+int porownaj (const void *a, const void *b) {
+	int which;
+	which = (*(int*)a - *(int*)b);
+	if (which>0) return 1;
+	else if (which == 0) return 0;
+	else return -1;
+	}
 
-for (i=0; i<n; i++) {
- r=rand()%101;
- table[i]=r;
-}
-	/* for (i=0; i<500; i++) {
- 	printf("%i\n",table[i]);
-	} */
-	start = clock();
-	quickSort( table, 0, n);
-	stop = clock();
-	czas_wykonania = (stop - start) ;
-	czas_sec = (long double)(czas_wykonania * 1000 / (CLOCKS_PER_SEC)) ;
-	printf ("Od startu zegara uplynelo %ld jednostek\n", clock ());
-  printf ("na jedna sekunde sklada sie %ld jednostek\n", CLOCKS_PER_SEC);
-	printf("Start T: %ld End T: %ld \t Czas Wykonania: %ld [ms]\n ",start, stop, czas_sec);
-	/*  for(i = 0; i < 501; ++i) {
-		printf(" %d ", table[i]);
-	} */
-	printf("\n");
+  /* Początek funkcji main */
+  int main (int argc, char** argv) {
+  /* Wyświetl komunikat o błędzie, jeśli nie podano 3 zmiennych */
+  if (argc<3) {
+    printf("Niepoprawny sposob wywolania programu. Jako argumenty wywolania podaj: 1) liczbe liczb losowych 2) liczbe przedzialow 3) sposob dzialania programu: 0/1 (0-quicksort z biblioteki, 1-quicksort z własnej implementacji)");
+  }
+  else {
+  int liczba_losowych, przedzialy, mode, i; /* mode - sposób działania programu */
 
-return 0;
-}
+  /* Konwersja argumentów wywołania programu */
+  char *error_argv1, *error_argv2, *error_argv3;
+  liczba_losowych = strtol(argv[1], &error_argv1, 10 );
+  przedzialy =  strtod(argv[2], &error_argv2);
+  mode = strtol(argv[3], &error_argv3, 10 );
+  	if ((error_argv1 || error_argv2 || error_argv3) == 0) {
+  		printf("Error. Konwersja nie powiodla sie, podaj jako argumenty wywolania programu liczby calkowite\n");
+  		printf("argv[1]=%s argv[2]=%s argv[3]=%s\n", error_argv1, error_argv2, error_argv3 );
+  		return -1;
+  	}
+  	else {
+
+  /* Deklaracja macierzy i czasów wykonania */
+  int macierz[liczba_losowych], r;
+  clock_t start, stop, czas_wykonania, czas_sec;
+  srand(time(NULL));
+
+  /* Generacja macierzy liczb pseudolosowych z użyciem rand */
+   for (i=0; i<liczba_losowych; i++) {
+     r = rand()%1000;
+     macierz[i] = r;
+     }
+
+  /* Sortowanie z użyciem qsort systemowy */
+  if (mode == 0) {
+    /* sortowanie i pomiar czasu */
+    start = clock();
+    qsort(macierz, liczba_losowych, sizeof(int), porownaj);
+    stop = clock();
+    czas_wykonania = (stop - start);
+    czas_sec = (long double)(czas_wykonania * 1000000 / (CLOCKS_PER_SEC));
+    printf("Czas wykonania sortowania %d-liczb: %ld [microsec]\n", liczba_losowych, czas_sec);
+    }
+  else if (mode == 1) {
+    start = clock();
+    quickSort(macierz, 0, liczba_losowych);
+    stop = clock();
+    czas_wykonania = (stop - start);
+    czas_sec = (long double)(czas_wykonania * 1000000 / (CLOCKS_PER_SEC));
+    printf("Czas wykonania sortowania %d-liczb: %ld [microsec]\n", liczba_losowych, czas_sec);
+    }
+  else return -1;
+  printf("\n");
+
+  /* Próbne wyświetlenia */
+  /* for (i=0; i<liczba_losowych; i++) {
+    printf(" %f ", macierz[i]);
+  }										*/
+
+  /* Zliczenia */
+  /* Wyzeruj tablicę liczby zliczeń */
+  int liczba_zliczen[przedzialy];
+  for (i=0; i<=(przedzialy +1); i++) {
+    liczba_zliczen[i] = 0;
+    }
+
+    int j;
+    i = 0;
+    for (j=1; j<=przedzialy; j++) {
+      while ((macierz[i] <= (1000/przedzialy) * j) && i<liczba_losowych) {
+  /*    printf("i= %d\tmacierz[i]= %f\tprzedzial= %f\n", i, macierz[i], (double) 1/przedzialy*j ); */
+        liczba_zliczen[j] = liczba_zliczen[j] + 1 ;
+        i++;
+      }
+    }
+
+  /* Wyświetlanie liczby zliczeń w danych przedziałach */
+  /* printf("\nLiczba zliczen w przedziale:\n");
+  for (i=1; i<=przedzialy; i++) {
+    printf(" %d = %d\n", i, liczba_zliczen[i]);
+  }			*/
+
+  /* Histogram */
+  /* Znajdż max_zliczen */
+  int max_zliczen = 0;
+  for (i=0; i<przedzialy; i++) {
+  	if (max_zliczen < liczba_zliczen[i]) {
+  		max_zliczen = liczba_zliczen[i];
+  	}
+  }
+  /* printf("max_zliczen=%d\nPrzedzial: \n",max_zliczen ); */
+  /* Wyświetl histogram */
+  for (i=1; i<=przedzialy; i++) {
+  	printf("%d:\t", i);
+  	int histo_dl = (78*liczba_zliczen[i])/max_zliczen;
+
+  	  for (j=0; j<=histo_dl; j++) {
+  		  printf("#");
+  	  }
+  	printf("\n" );
+  }
+   } /* koniec else po konwersji */
+
+  printf("\n");
+  } /* Koniec else po wywolaniu */
+  return 0;
+  }
 
 void quickSort( int a[], int l, int r)
 {
@@ -41,15 +127,13 @@ void quickSort( int a[], int l, int r)
 
    if( l < r )
    {
-   	/* divide and conquer */
-        j = partition( a, l, r);
+        j = podziel( a, l, r);
        quickSort( a, l, j-1);
        quickSort( a, j+1, r);
    }
-
 }
 
-int partition( int a[], int l, int r) {
+int podziel( int a[], int l, int r) {
    int pivot, i, j, t;
    pivot = a[l];
    i = l; j = r+1;
