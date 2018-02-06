d887addf29c550ba881290005121c0ed575e6d62
Lukasz Strzelec <lukastrzelec@gmail.com>
2018-02-06 19:51:10 +0100

    Initial pomoc and program

---
+++ zal/dummyencryptor.c
@@ -0,0 +1,15 @@
+/**
+    Łukasz Strzelec
+    Program zaliczeniowy
+    DummyEncryptor
+**/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main (){
+fprintf(stdout, "Chcesz zaszyforwać wiadomość, czy odszyfrować?\nWybierz sposób działania:\n(wpisz swoj wybor)\n [E] - Zaszyfruj \t [D] - Odszyfruj\n" );
+  
+}
