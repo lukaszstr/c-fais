06c96da56cb66744ff47afcd7c910f04dcceafa8
lukaszstr <lukastrzelec@gmail.com>
2018-10-24 16:38:02 +0200

    a Program zdaje aie dziala

--- prog3/dwumian-newtona.c
+++ prog3/dwumian-newtona.c
@@ -27,7 +27,7 @@ double dn (double a, double b, unsigned n)
 {
 			int i;
 			double wynik=0.0;
-			for (int i = 0; i <= n; i++)
+			for (i = 0; i <= n; i++)
 			{
 				printf(" %g +",(sn(n,i) * pow(a,i) * pow(b, n-i)) );
 				wynik += sn(n,i) * pow(a,i) * pow(b, n-i);
@@ -45,17 +45,17 @@ int main()
 	printf("b = ");
 	scanf("%lf", &b);
 	printf("n = ");
-	scanf("%d", &n);
+	scanf("%u", &n);
 /*	if (n<k) 
 	{
 		printf("n musi byc wieksze od k\n");
 		return -1;
 	}
 */
-	printf("DN(%d,%d)^%d = ",a,b,n);
+	printf("DN(%g,%g)^%d = ",a,b,n);
 	dn(a,b,n);
 	printf(" \n ");
-	printf("(%d+%d)^%d = %g\n",a,b,n, lewastrona(a,b,n));
+	printf("(%g+%g)^%d = %g\n",a,b,n, lewastrona(a,b,n));
 
 	return 0;
 
